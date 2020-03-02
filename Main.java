b75fa11d5a58c98589ffeb1f30db56ebc24b4f8e
AlexSCalev <60624475+AlexSCalev@users.noreply.github.com>
2020-03-02 09:58:03 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Source;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Select method parsing file and set away to file.
+	csv_parsing objectParser = new csv_parsing();
+//	 splitParsing indicate you awayToFile
+//	objectParser.splitParsing();
+//	Example use ScannerParsing
+//	objectParser.ScannerParsing("C:\\Users\\Alexandr\\Desktop\\CSV Generate File\\bublic.csv");
+    }
+}
